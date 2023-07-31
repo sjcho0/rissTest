611c5c6b6a98a187e3f3ef1408883c9a76da2a58
sjcho0 <130718230+sjcho0@users.noreply.github.com>
2023-07-31 10:14:39 +0900

    Update common.js

--- script/commons/common.js
+++ script/commons/common.js
@@ -162,6 +162,7 @@ $('.searchClose').click(function(){
   $('html, body').css('overflow','visible');
   $('.blackBg1').hide();
   $('#divSearch').removeClass('on');
+  $('#divSearch').hide();
   // $('#divSearch').animate({"bottom":"-100%"})
   return false;
 });
